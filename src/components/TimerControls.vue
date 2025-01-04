fa539a8143388f17decf274517f2311ea28a2bcd
BagasMiftahun <bagasmiftahunaim@gmail.com>
2025-01-04 18:03:48 +0700

    style: :sparkles: add shadcn ui library

--- src/components/TimerControls.vue
+++ src/components/TimerControls.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="flex justify-center gap-4">
-      <button @click="toggleTimer" :class="isRunning ? 'bg-yellow-500' : 'bg-green-500'" class="text-white px-4 py-2 rounded">
+      <Button @click="toggleTimer" :class="isRunning ? 'bg-yellow-500' : 'bg-green-500'" class="text-white px-4 py-2 rounded">
         {{ isRunning ? 'Pause' : 'Start' }}
-      </button>
+      </Button>
       <button @click="resetTimer" class="bg-red-500 text-white px-4 py-2 rounded">Reset</button>
     </div>
   </template>
   
   <script lang="ts">
   import { defineComponent } from 'vue';
+  import { Button } from './ui/button';
   
   export default defineComponent({
     props: {
